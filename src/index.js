bb8dc43b6dcfb8e653303c1797ee302270116c3f
Lucas David Erkana <lderkana21x@gmail.com>
2023-01-16 17:50:15 +0200

    Add jsx to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react"
 import ReactDOM from "react-dom"
-//component file
+
+import { BrowserRouter as Router } from "react-router-dom";
+
+//component
 import TodoContainer from "./components/TodoContainer"
+
+//stylesheet
+import "./App.css"
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoContainer />
-  </React.StrictMode>,
+    <Router basename={process.env.PUBLIC_URL}>
+      <TodoContainer />
+    </Router>  
+  </React.StrictMode>, 
   document.getElementById("root")
-)
+);
